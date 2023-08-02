fe274426e6968e4aeec1fbbe9aea04d76afd13d5
ronaega <ronaegakharismaaa@gmail.com>
2023-08-02 22:45:39 +0700

    yes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ window.addEventListener("scroll", function () {
   let offset =  window.pageYOffset;
   parallax.style.backgroundPositionY = offset * 1 + 'px';
 });
+
+var sidemenu = document.getElementById("media-header");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
