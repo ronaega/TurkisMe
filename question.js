e69ec86dcb037bf3e2b0f764ccb117fdbdb395a4
ronaega <ronaegakharismaaa@gmail.com>
2023-08-08 21:40:20 +0700

    yes

---
+++ question.js
@@ -0,0 +1,114 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Beni seviyor musun?",
+        answer: "C. Evet",
+        options: [
+            "A. Bilmiyorum",
+            "B. Düşünüyorum",
+            "C. Evet",
+            "D. Hayır"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Benimle evlenir misin?",
+        answer: "B. Evet",
+        options: [
+            "A. Düşünüyorum",
+            "B. Evet",
+            "C. Hayır",
+            "D. Bilmiyorum"   
+        ]
+    },
+    {
+        numb: 3,
+        question: "Benimle yaşlanır mısır?",
+        answer: "C. Evet",
+        options: [
+            "A. Bilmiyorum",
+            "B. Hayır",
+            "C. Evet",
+            "D. Düşünüyorum"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Her sabah benim yanımda uyanır mısın?",
+        answer: "D. Evet",
+        options: [
+            "A. Hayır",
+            "B. Düşünüyorum",
+            "C. Bilmiyorum",
+            "D. Evet"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Benimle beraber cennete gider misin?",
+        answer: "A. Evet",
+        options: [
+            
+            "A. Evet",
+            "B. Düşünüyorum",
+            "C. Hayır",
+            "D. Bilmiyorum"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Beni seviyor musun?",
+        answer: "C. Evet",
+        options: [
+            "A. Bilmiyorum",
+            "B. Düşünüyorum",
+            "C. Evet",
+            "D. Hayır"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Benimle evlenir misin?",
+        answer: "B. Evet",
+        options: [
+            "A. Düşünüyorum",
+            "B. Evet",
+            "C. Hayır",
+            "D. Bilmiyorum"   
+        ]
+    },
+    {
+        numb: 8,
+        question: "Benimle yaşlanır mısır?",
+        answer: "C. Evet",
+        options: [
+            "A. Bilmiyorum",
+            "B. Hayır",
+            "C. Evet",
+            "D. Düşünüyorum"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Her sabah benim yanımda uyanır mısın?",
+        answer: "D. Evet",
+        options: [
+            "A. Hayır",
+            "B. Düşünüyorum",
+            "C. Bilmiyorum",
+            "D. Evet"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Benimle beraber cennete gider misin?",
+        answer: "A. Evet",
+        options: [
+            
+            "A. Evet",
+            "B. Düşünüyorum",
+            "C. Hayır",
+            "D. Bilmiyorum"
+        ]
+    }
+];
